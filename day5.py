60c649b119c515f79d4323cbe58ffe3c1388c05c
Matthias Plappert <matthiasplappert@me.com>
2021-12-08 12:22:04 -0800

    Remove unnecessary normalization since we need to handle it for diagonal anyway

--- day5.py
+++ day5.py
@@ -9,17 +9,9 @@ def parse_line(line):
     return ((int(x), int(y)), (int(x_), int(y_)))
 
 
-def normalize_line(line):
-    lhs, rhs = line
-    if np.any(rhs - lhs < 0):
-        return rhs, lhs
-    return lhs, rhs
-
-
 # Parse data.
 with open("day5.txt", "r") as f:
     data = np.array([parse_line(l) for l in f.readlines()])
-    data = np.array([normalize_line(l) for l in data])
 print(data.shape)
 
 # Find maximum of coordinate system (origin is always (0, 0)).
